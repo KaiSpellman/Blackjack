66062f4bbb725bff602dcc9d391dfc4ed46de14c
Kai Spellman <kai.spellman03@gmail.com>
2023-12-05 17:03:50 -0800

    implemented single deck of cards and shuffle

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include "card.cpp"
 
 //Bet $2-$500
@@ -19,16 +20,31 @@ void createDeck(vector<Card>* deck) {
 }
 
 void swap(vector<Card>* deck, int idx1, int idx2) {
-
+    Card temp = (*deck).at(idx1);
+    (*deck).at(idx1) = (*deck).at(idx2);
+    (*deck).at(idx2) = temp;
 }
 
 void shuffleDeck(vector<Card>* deck) {
+    int n = deck->size();
 
+    for (int i = n-1; i > 0; --i)
+    {
+        int j = rand() % (i + 1);
+        swap(deck,i,j);
+    }
 }
 
 int main() {
     vector<Card>* deck = new vector<Card>;
     createDeck(deck);
+    shuffleDeck(deck);
+    for (int i = 0; i < 52; i++)
+    {
+        cout << deck->at(i).getValue() << " " << deck->at(i).getSuit() << endl;
+    }
+    
+    
     delete deck;
 
     return 0;
